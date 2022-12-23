e36b005e054e2c9b1fe169a0554ea77a10e80a32
Mantas <Mantasb77@gmail.com>
2022-12-23 16:43:50 +0000

    Added RepositoryModule

---
+++ app/src/main/java/com/kotlisoft/clouderity/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.kotlisoft.clouderity.di
+
+import com.kotlisoft.clouderity.data.repository.WeatherRepositoryImpl
+import com.kotlisoft.clouderity.domain.repository.WeatherRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(
+        weatherRepositoryImpl: WeatherRepositoryImpl
+    ): WeatherRepository
+}
