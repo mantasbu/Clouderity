69c2a03b7027c1cfe92cfee4b756779a7e0f7f7c
Mantas <Mantasb77@gmail.com>
2022-12-23 13:55:45 +0000

    Renamed method to getWeatherData

--- app/src/main/java/com/kotlisoft/clouderity/data/remote/WeatherApi.kt
+++ app/src/main/java/com/kotlisoft/clouderity/data/remote/WeatherApi.kt
@@ -5,7 +5,7 @@ import retrofit2.http.Query
 
 interface WeatherApi {
     @GET("v1/forecast?current_weather=true&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,cloudcover,windspeed_10m,weathercode&timezone=auto&daily=temperature_2m_min,temperature_2m_max,weathercode,sunrise,sunset,windspeed_10m_max")
-    suspend fun getHourlyWeatherData(
+    suspend fun getWeatherData(
         @Query("latitude") latitude: Double,
         @Query("longitude") longitude: Double
     ): WeatherDto
